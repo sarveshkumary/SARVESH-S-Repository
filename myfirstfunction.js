34ffa8c9d4aa3989df7dc3d4e186d6974bebd057
sarveshkumary <77781472+sarveshkumary@users.noreply.github.com>
2021-05-13 03:55:46 +0100

    Add files via upload

---
+++ myfirstfunction.js
@@ -0,0 +1,12 @@
+function changeImage(){
+var image = document.getElementById('myImage');
+
+if
+
+(image.src.match("bitcoinmarket"))
+
+{image.src="bitcoin.jpg";}
+else
+{image.src="bitcoinmarket.jpg";}
+
+}
